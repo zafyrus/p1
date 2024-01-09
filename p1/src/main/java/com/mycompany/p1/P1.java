541a144f397915839353db21666d81e26b4b4871
Asanchezb <zafyru23@gmail.com>
2024-01-09 10:42:04 +0100

    añadiendo segundo commit

---
+++ p1/src/main/java/com/mycompany/p1/P1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p1;
+
+/**
+ *
+ * @author Zafyrus
+ */
+public class P1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Adios");
+    }
+}
